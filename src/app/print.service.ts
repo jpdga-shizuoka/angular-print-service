5e8c8a1b16e89f570220112842f105b87f8b1ee2
jpdga-shizuoka <56290481+jpdga-shizuoka@users.noreply.github.com>
2020-11-03 10:47:13 +0900

    working good with safari & chrome on iOS, but...

--- src/app/print.service.ts
+++ src/app/print.service.ts
@@ -1,27 +1,41 @@
-import { Injectable } from '@angular/core';
+import { Injectable, OnDestroy } from '@angular/core';
 import {Router} from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
-export class PrintService {
+export class PrintService implements OnDestroy {
+  private static instance: PrintService;
+  private static beforeprint(event: Event) {
+    PrintService.instance.isPrinting = true;
+  }
+  private static afterprint(event: Event) {
+    PrintService.instance.isPrinting = false;
+    PrintService.instance.router.navigate([{ outlets: { print: null }}]);
+  }
+
   isPrinting = false;
 
-  constructor(private router: Router) { }
+  constructor(private router: Router) {
+    PrintService.instance = this;
+    window.addEventListener('beforeprint', PrintService.beforeprint);
+    window.addEventListener('afterprint', PrintService.afterprint);
+  }
+
+  ngOnDestroy() {
+    window.removeEventListener('afterprint', PrintService.afterprint);
+    window.removeEventListener('beforeprint', PrintService.beforeprint);
+  }
 
   printDocument(documentName: string, documentData: string[]) {
-    this.isPrinting = true;
     this.router.navigate(['/',
       { outlets: {
         'print': ['print', documentName, documentData.join()]
-      }}]);
+      }}],
+      { skipLocationChange: true });
   }
 
   onDataReady() {
-    setTimeout(() => {
-      window.print();
-      this.isPrinting = false;
-      this.router.navigate([{ outlets: { print: null }}]);
-    });
+    setTimeout(() => window.print());
   }
 }
