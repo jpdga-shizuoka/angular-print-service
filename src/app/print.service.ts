135f95f73e6a0a0def3f8c66910689ea274e0d98
jpdga-shizuoka <56290481+jpdga-shizuoka@users.noreply.github.com>
2020-11-04 19:52:27 +0900

    working with Chrome & Safari on Android, iOS, macOS (#1)
    * working good with safari & chrome on iOS, but...
    * working good with chrome on macOS, but still...
    * update  @angular@^10
    * working good with chrome on Android as well

--- src/app/print.service.ts
+++ src/app/print.service.ts
@@ -1,27 +1,52 @@
-import { Injectable } from '@angular/core';
+import { Injectable, OnDestroy } from '@angular/core';
 import {Router} from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
-export class PrintService {
-  isPrinting = false;
+export class PrintService implements OnDestroy {
+  private static instance: PrintService;
+  private static afterprint(event: Event) {
+    PrintService.instance.afterprint();
+  }
+  private state: 'idle' | 'done' | 'complete' = 'complete';
 
-  constructor(private router: Router) { }
+  constructor(private router: Router) {
+    PrintService.instance = this;
+    window.addEventListener('afterprint', PrintService.afterprint);
+  }
+
+  ngOnDestroy() {
+    window.removeEventListener('afterprint', PrintService.afterprint);
+  }
 
   printDocument(documentName: string, documentData: string[]) {
-    this.isPrinting = true;
+    this.state = 'idle';
     this.router.navigate(['/',
       { outlets: {
         'print': ['print', documentName, documentData.join()]
-      }}]);
+      }}],
+      { skipLocationChange: true });
   }
 
   onDataReady() {
     setTimeout(() => {
       window.print();
-      this.isPrinting = false;
-      this.router.navigate([{ outlets: { print: null }}]);
+      this.afterprint();
     });
+  }
+
+  private afterprint() {
+    switch (this.state) {
+      case 'idle':
+        return this.state = 'done';
+      case 'done':
+        this.state = 'complete';
+        return this.endOfPrint();
+    }
+  }
+
+  private endOfPrint() {
+    setTimeout(() => this.router.navigate([{ outlets: { print: null }}]), 1000); // any good idea?
   }
 }
